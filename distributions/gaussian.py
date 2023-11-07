9b20e1e5feacb645dcdcd3cf0c6a1c37fba969d2
ferduarteee <ferfariaduarte@gmail.com>
2023-11-06 22:43:40 -0300

    main

--- distributions/gaussian.py
+++ distributions/gaussian.py
@@ -4,22 +4,26 @@ import plotly.express as px
 
 def gaussian(x, mi, var):
     lista = list()
-    for i in range(x):
+    x_label = []
+    for i in range(-x, x+1):
+        x_label.append(i)
         try:
             fx = ((math.e ** (-0.5 * (((i - mi) / var) ** 2))) / ((2 * math.pi * (var ** 2)) ** 0.5))
             lista.append(fx)
         except:
             fx = 0
-    fig = px.line(lista, width=900, height=500, title = 'Função de Densidade Normal', labels={'value': 'Probabilidade', 'index':'Eventos', 'variable':'Probabilidade'}, line_shape="spline")
+    fig = px.line(x = x_label,y=lista, width=900, height=500, title = 'Função de Densidade Normal', labels={'value': 'Probabilidade', 'index':'Eventos', 'variable':'Probabilidade'}, line_shape="spline")
     fig.update_traces(marker={'color': 'green'})
     return fig 
 
 def ac_gaussian(x, mi, var):
     lista = list()
     fx = 0
-    for i in range(x):
+    x_label = []
+    for i in range(-x, x+1):
+        x_label.append(i)
         fx = fx + ((math.e ** (-0.5 * (((i - mi) / var) ** 2))) / ((2 * math.pi * (var ** 2)) ** 0.5))
         lista.append(fx)
-    fig = px.line(lista, width=900, height=500, title = 'Função de Distribuição Normal', labels={'value': 'Probabilidade', 'index':'Eventos', 'variable':'Probabilidade'}, line_shape='hv')
+    fig = px.line(x=x_label,y=lista, width=900, height=500, title = 'Função de Distribuição Normal', labels={'value': 'Probabilidade', 'index':'Eventos', 'variable':'Probabilidade'}, line_shape='hv')
     fig.update_traces(marker={'color': 'green'})
     return fig
--- distributions/norm_def.py
+++ distributions/norm_def.py
@@ -7,7 +7,9 @@ def norm_pad(n, mean, devPad):
 # media = 25
 # devpad = 7
     lista = []
-    for i in range(1, n):
+    x_label = []
+    for i in range(-n, n+1):
+        x_label.append(i)
         try:
             z = (i - mean) / (devPad)
             valor = (1 / ((2 * math.pi) ** 1/2)) * (math.e ** (-1/2 * (z ** 2)))
@@ -16,14 +18,16 @@ def norm_pad(n, mean, devPad):
             z = 0
             valor = 0 
             lista.append(valor)
-    fig = px.line(lista, width=800, height=500, title = 'Função de Probabilidade Normal Padrão', labels={'value': 'Probabilidade', 'index':'Eventos', 'variable': 'Probabilidade'}, line_shape='spline')
+    fig = px.line(x=x_label,y=lista, width=800, height=500, title = 'Função de Probabilidade Normal Padrão', labels={'value': 'Probabilidade', 'index':'Eventos', 'variable': 'Probabilidade'}, line_shape='spline')
     fig.update_traces(marker={'color': 'green'})
     return fig 
 
 def norm_pad_accum(n, mean, devPad):
     lista = []
+    x_label=[]
     valor = 0
-    for i in range(1, n):
+    for i in range(-n, n+1):
+        x_label.append(i)
         try:
             z = (i - mean) / (devPad)
             valor = valor + (1 / ((2 * math.pi) ** 1/2)) * (math.e ** (-1/2 * (z ** 2)))
@@ -32,6 +36,6 @@ def norm_pad_accum(n, mean, devPad):
             z = 0
             valor = 0 
             lista.append(valor)
-    fig = px.line(lista, width=900, height=500, title = 'Função de Distribuição Normal Padrão', labels={'value': 'Probabilidade', 'index':'Eventos', 'variable':'Probabilidade'}, line_shape='hv')
+    fig = px.line(x=x_label,y=lista, width=900, height=500, title = 'Função de Distribuição Normal Padrão', labels={'value': 'Probabilidade', 'index':'Eventos', 'variable':'Probabilidade'}, line_shape='hv')
     fig.update_traces(line_color='green')
     return fig
